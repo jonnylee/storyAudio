cfb0386a5f45bac1216a440e168626443b6856ee
jonny <whoisjonny@outlook.com>
2015-09-24 23:36:23 +0800

    init
    init

---
+++ storyAudio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  storyAudio
+//
+//  Created by LiuJianxun on 9/24/15.
+//  Copyright (c) 2015 jonny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
